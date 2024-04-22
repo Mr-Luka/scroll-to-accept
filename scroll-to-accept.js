0e09c25aea49b6b105db060ffb74fe882a6d894f
Luka <luka.luka.luka93@gmail.com>
2024-04-22 13:00:37 -0700

    Making a function thats gonna observe the button

--- scroll-to-accept.js
+++ scroll-to-accept.js
@@ -1,3 +1,11 @@
 const terms = document.querySelector('.terms-and-conditions');
 const watch = document.querySelector('.watch');
 const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
